9794a18d1613aa2ebc9673fca714d3c5e59f2933
Lindy <lindy.wilkins@gmail.com>
2017-07-28 13:11:23 -0400

    vars

--- OSCInstallation.ino
+++ OSCInstallation.ino
@@ -1,30 +1,69 @@
+// Example sketch to read the ID from an Addicore 13.56MHz RFID tag
+// as found in the RFID AddiKit found at:
+// http://www.addicore.com/RFID-AddiKit-with-RC522-MIFARE-Module-RFID-Cards-p/126.htm
+
 #include <AddicoreRFID.h>
 #include <SPI.h>
 #include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
 #include <avr/power.h>
+#endif
 #define PIN 6
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, PIN, NEO_GRB + NEO_KHZ800);
+
 #define  uchar unsigned char
 #define uint  unsigned int
-int TotalSteps = 10;
-int fadeRate = 10;
-int currTag;
+
 uchar fifobytes;
 uchar fifoValue;
-AddicoreRFID myRFID;
+int add;
+
+AddicoreRFID myRFID; // create AddicoreRFID object to control the RFID module
+
+/////////////////////////////////////////////////////////////////////
+//set the pins
+/////////////////////////////////////////////////////////////////////
 const int chipSelectPin = 10;
 const int NRSTPD = 5;
+
+//Maximum length of the array
 #define MAX_LEN 16
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(300, PIN, NEO_GRB + NEO_KHZ800);
+
+int currTag;
+
+int vars[] = { 216, 228, 227, 203, 229, 238, 215,
+248, 239, 257, 228, 281, 238, 237, 248, 260, 204,
+195, 206, 205 };
+//int oil = 228;
+//int lang = 216;
+//int immg = 227;
+//int beaver = 203;
+//int syrup = 229;
+//int coffee = 238;
+//int sport = 215;
+//int ethnic = 248;
+//int donut = 239;
+//int lights = 257;
+//int rural
+//int immg
+//int festival
+//int 
+
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600);                        // RFID reader SOUT pin connected to Serial RX pin at 9600bps
+
+  // start the SPI library:
   SPI.begin();
+
   strip.begin();
-  strip.show(); 
-  pinMode(chipSelectPin, OUTPUT);             
-  digitalWrite(chipSelectPin, LOW);        
-  pinMode(NRSTPD, OUTPUT);                   
+  strip.show(); // Initialize all pixels to 'off'
+  pinMode(chipSelectPin, OUTPUT);             // Set digital pin 10 as OUTPUT to connect it to the RFID /ENABLE pin
+  digitalWrite(chipSelectPin, LOW);         // Activate the RFID reader
+  pinMode(NRSTPD, OUTPUT);                    // Set digital pin 10 , Not Reset and Power-down
   digitalWrite(NRSTPD, HIGH);
+
   myRFID.AddicoreRFID_Init();
+  Serial.println("finished init");
 }
 
 void loop()
@@ -33,31 +72,34 @@ void loop()
   uchar status;
   uchar str[MAX_LEN];
   uchar RC_size;
-  uchar blockAddr;
-  String mynum = "";
+  uchar blockAddr;  //Selection operation block address 0 to 63
+
   str[1] = 0x4400;
+  //Find tags, return tag type
   status = myRFID.AddicoreRFID_Request(PICC_REQIDL, str);
   if (status == MI_OK)
-  {
     Serial.println("RFID tag detected");
-    uint tagType = str[0] << 8;
+  status = myRFID.AddicoreRFID_Anticoll(str);
+  if (status == MI_OK)
+  {
+    add = str[0] + str[1] + str[2] + str[3];
+    Serial.println(add);
+    myRFID.AddicoreRFID_Halt();
+  }
+  myRFID.AddicoreRFID_Halt();      //Command tag into hibernation
+  if (add == vars[0]) {
+    colorWipe(strip.Color(255, 0, 0), 50); // Red
+  }
+  if (add == vars[1]) {
+    colorWipe(strip.Color(255, 255, 0), 50); // Red
   }
 
-  myRFID.AddicoreRFID_Halt(); 
-  if (int(currTag) == 233) { // If the tag is this
-    //colorWipe(strip.Color(255, 0, 0), 5); // Red
-
-    // There are 300 LEDs, starting at 0
-    setSection(0, 200, 255, 0, 0); // Set 0 to 200 RED
-    setSection(201, 299,0, 255, 0); // Set the rest GREEN
-    
-  } // End each student code
-  if (int(currTag) == 234) {
-    //colorWipe(strip.Color(0, 255, 0), 0); // Green
-  }
-  if (int(currTag) == 246) {
-    //colorWipe(strip.Color(0, 0, 255), 0); // Blue
-  }
+}
+//pretty self explanatory, cards should be saved in a uchar array like the variable 'blue' above
+bool compareTag(uchar from[], uchar to[]) {
+  if (from[0] == to[0] && from[1] == to[1] && from[2] == to[2] && from[3] == to[3])
+    return true;
+  return false;
 }
 
 void colorWipe(uint32_t c, uint8_t wait) {
@@ -68,23 +110,3 @@ void colorWipe(uint32_t c, uint8_t wait) {
   }
 }
 
-void setSection(int start, int finish, int Nred, int Ngreen, int Nblue) {
-  uint32_t c = strip.getPixelColor(start);
-  uint8_t  redCur = (c >> 16) & 0xFF;
-  uint8_t  greenCur = (c >>  8) & 0xFF;
-  uint8_t  blueCur = c & 0xFF;
-  uint8_t redNew = Nred;
-  uint8_t greenNew = Ngreen;
-  uint8_t blueNew = Nblue;
-  for (int i = 1; i < TotalSteps; i++)
-  {
-    uint8_t red = (((redCur * (TotalSteps - i)) + (redNew * i)) / TotalSteps);
-    uint8_t green = (((greenCur * (TotalSteps - i)) + (greenNew * i)) / TotalSteps);
-    uint8_t blue = (((blueCur * (TotalSteps - i)) + (blueNew * i)) / TotalSteps);
-    for (int j = start; j < finish + 1; j++) {
-      strip.setPixelColor(j, red, green, blue);
-      strip.show();
-      delay(fadeRate);
-    }
-  }
-}
